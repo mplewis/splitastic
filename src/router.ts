d3cb3695eee38a790a8baab6465b0d86b488065a
Matt Lewis <matt@mplewis.com>
2019-02-13 17:29:28 -0500

    chore: prettier all

--- src/router.ts
+++ src/router.ts
@@ -1,23 +1,24 @@
-import Vue from 'vue';
-import Router from 'vue-router';
-import Home from './views/Home.vue';
+import Vue from 'vue'
+import Router from 'vue-router'
+import Home from './views/Home.vue'
 
-Vue.use(Router);
+Vue.use(Router)
 
 export default new Router({
   routes: [
     {
-      path: '/',
-      name: 'home',
       component: Home,
+      name: 'home',
+      path: '/'
     },
     {
-      path: '/about',
-      name: 'about',
       // route level code-splitting
       // this generates a separate chunk (about.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "about" */ './views/About.vue'),
-    },
-  ],
-});
+      component: () =>
+        import(/* webpackChunkName: "about" */ './views/About.vue'),
+      name: 'about',
+      path: '/about'
+    }
+  ]
+})
