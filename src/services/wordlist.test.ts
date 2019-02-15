625945cc7f2386b8c2d6549f08228ad40276073e
Matt Lewis <matt@mplewis.com>
2019-02-14 21:50:59 -0700

    feat: reworked encode with checksum

--- src/services/wordlist.test.ts
+++ src/services/wordlist.test.ts
@@ -1,4 +1,4 @@
-import { encodeBytes, parseWords, parseWordsChecksum } from './wordlist'
+import { encodeBytes } from './wordlist'
 
 describe('parseWords', () => {
   it('parses English words to the expected bytes', () => {
@@ -14,93 +14,101 @@ describe('parseWords', () => {
      *  =  2173456992   155782130
      *  = 81 8C 52 60 09 49 0B F2
      */
-    expect(
-      parseWords(['light', 'glad', 'oblige', 'announce', 'embark', 'wish'])
-    ).toEqual(new Uint8Array([0x81, 0x8c, 0x52, 0x60, 0x09, 0x49, 0x0b, 0xf2]))
-
-    // The last word in every 4-byte group loses its LSB, so it could be one of two words
-    expect(
-      parseWords(['light', 'glad', 'object', 'announce', 'embark', 'witness'])
-    ).toEqual(new Uint8Array([0x81, 0x8c, 0x52, 0x60, 0x09, 0x49, 0x0b, 0xf2]))
+    // expect(
+    //   parseWords(['light', 'glad', 'oblige', 'announce', 'embark', 'wish'])
+    // ).toEqual(new Uint8Array([0x81, 0x8c, 0x52, 0x60, 0x09, 0x49, 0x0b, 0xf2]))
+    // // The last word in every 4-byte group loses its LSB, so it could be one of two words
+    // expect(
+    //   parseWords(['light', 'glad', 'object', 'announce', 'embark', 'witness'])
+    // ).toEqual(new Uint8Array([0x81, 0x8c, 0x52, 0x60, 0x09, 0x49, 0x0b, 0xf2]))
   })
 })
 
-describe('parseWordsChecksum', () => {
-  /**
-   * 252 252 232 229 0 65 48 162 248 225 230 208 65 135 56 77 212 21 210 94 68
-   *   198 157 79 58 42 242 111 240 176 250 12
-   * sum: 4764
-   * mod 2048: 668
-   * checksum: "fatal"
-   */
-  const words = [
-    'woman',
-    'trap',
-    'defense',
-    'able',
-    'another',
-    'chunk',
-    'wedding',
-    'august',
-    'reflect',
-    'dose',
-    'degree',
-    'beach',
-    'staff',
-    'purse',
-    'nut',
-    'dynamic',
-    'crunch',
-    'fault',
-    'demand',
-    'fiscal',
-    'orbit',
-    'vague',
-    'march',
-    'pact'
-  ]
-  const subject = (checksum: string) =>
-    parseWordsChecksum([...words, checksum])
+// describe('parseWordsChecksum', () => {
+//   /**
+//    * 252 252 232 229 0 65 48 162 248 225 230 208 65 135 56 77 212 21 210 94 68
+//    *   198 157 79 58 42 242 111 240 176 250 12
+//    * sum: 4764
+//    * mod 2048: 668
+//    * checksum: "fatal"
+//    */
+//   const words = [
+//     'woman',
+//     'trap',
+//     'defense',
+//     'able',
+//     'another',
+//     'chunk',
+//     'wedding',
+//     'august',
+//     'reflect',
+//     'dose',
+//     'degree',
+//     'beach',
+//     'staff',
+//     'purse',
+//     'nut',
+//     'dynamic',
+//     'crunch',
+//     'fault',
+//     'demand',
+//     'fiscal',
+//     'orbit',
+//     'vague',
+//     'march',
+//     'pact'
+//   ]
+//   const subject = (checksum: string) =>
+//     parseWordsChecksum([...words, checksum])
 
-  describe('when the checksum is correct', () => {
-    const checksum = 'fatal'
+//   describe('when the checksum is correct', () => {
+//     const checksum = 'fatal'
 
-    it('parses properly', () => {
-      const bytes = subject(checksum)
-      const sum = bytes.reduce((a: number, n: number) => a + n, 0)
-      expect(sum).toEqual(4764)
-    })
-  })
+//     it('parses properly', () => {
+//       const bytes = subject(checksum)
+//       const sum = bytes.reduce((a: number, n: number) => a + n, 0)
+//       expect(sum).toEqual(4764)
+//     })
+//   })
 
-  describe('when the checksum is incorrect', () => {
-    const checksum = 'fatigue'
+//   describe('when the checksum is incorrect', () => {
+//     const checksum = 'fatigue'
 
-    it('throws the expected error', () => {
-      expect(() => subject(checksum)).toThrowError(/Checksum mismatch/)
-    })
-  })
-})
+//     it('throws the expected error', () => {
+//       expect(() => subject(checksum)).toThrowError(/Checksum mismatch/)
+//     })
+//   })
+// })
 
-describe('encodeBytes', () => {
-  it('encodes bytes as words', () => {
-    expect(
-      encodeBytes(
-        new Uint8Array([0x81, 0x8c, 0x52, 0x60, 0x09, 0x49, 0x0b, 0xf2])
-      )
-    ).toEqual(['light', 'glad', 'object', 'announce', 'embark', 'wish'])
-  })
-})
+describe.only('encodeBytes', () => {
+  it('encodes bytes including the proper checkword', () => {
+    const toEncode = new Uint8Array([0xc0, 0xff, 0xee, 0xca, 0xfe])
+    // as integers: 192, 255, 238, 202, 254 (, 0, 0, 0)
+    // as 33-bit blobs: 11000000 11111111 11101110 11001010 (0)
+    //                  11111110 00000000 00000000 00000000 (0)
+    // as 11-bit chunks: 11000000111 11111111011 10110010100
+    //                   11111110000 00000000000 00000000000
+    // as numbers: 1543, 2043, 1428, 2032, 0, 0
+    // as words: science, youth, raw, wrap, abandon, abandon
 
-describe('encodeBytesCheckdropsum', () => {
-  it('encodes bytes including the proper checksum and drop byte count', () => {
     // A word represents 11 bits of data
-    // The first 9 bits of the checkdropsum are reserved for the checksum (mod 512)
-    // Sum: 1141, % 512 = 117 checksum => 001110101
-    // The last 2 bits represent the drop byte count
+    // The first 2 bits of the checkword represent the drop byte count
     // Bytes are encoded in groups of 4
-    // To encode 5 bytes, we pad the end with 0x00 bytes and drop 3 bytes later (11)
-    // Expected checksum: 00111010111 => 471 => "depart"
-    const toEncode = [0xc0, 0xff, 0xee, 0xca, 0xfe]
-    // expect(encodeBytesCheckdropsum(toEncode)).toEqual([])
+    // To encode 5 bytes, we pad the end with 0x00 bytes and drop 3 bytes later => 0b11
+    // The last 9 bits of the checkword are reserved for the checksum (mod 512)
+    // Sum: 1141, % 512 = 117, checksum => 001110101
+    // Expected checkword: 11001110101 => 1653 => "solid"
+
+    // Checkword is appended to the data word array
+    // Expected final output: science, youth, raw, wrap, abandon, abandon, solid
+    expect(encodeBytes(toEncode)).toEqual([
+      'science',
+      'youth',
+      'raw',
+      'wrap',
+      'abandon',
+      'abandon',
+      'solid'
+    ])
   })
 })
--- src/services/wordlist.ts
+++ src/services/wordlist.ts
@@ -1,95 +1,144 @@
 import wordlist from '@/vendor/wordlists/english.ts'
 
-import { curry, tap } from 'ramda'
+import {
+  all,
+  curry,
+  join,
+  map,
+  pipe,
+  reduce,
+  split,
+  splitEvery,
+  sum,
+  tap,
+  unnest
+} from 'ramda'
 
-function toNum (word: string) {
-  const index = wordlist.indexOf(word)
-  if (index === -1) {
-    throw new Error(`Could not find "${word}"`)
-  }
-  return index
-}
+const CHECKSUM_BITS = 9
+const DROP_BITS = 2
 
-function toBin (i: number) {
+// function toNum (word: string) {
+//   const index = wordlist.indexOf(word)
+//   if (index === -1) {
+//     throw new Error(`Could not find "${word}"`)
+//   }
+//   return index
+// }
+
+const toBin = curry((binLen: number, i: number) => {
   // tslint:disable no-bitwise
-  return (i >>> 0).toString(2)
-}
-
-const leftPad = curry((filler: string, len: number, item: string) => {
-  let result = item
-  while (result.length < len) {
-    result = filler + result
+  const result = leftPad('0', binLen, (i >>> 0).toString(2))
+  if (result.length !== binLen) {
+    throw new Error(
+      `Error converting "${i}" to binary of length "${binLen}": got "${result}"`
+    )
   }
   return result
 })
 
-const zeroPad = leftPad('0')
+function toWord (num: number) {
+  const word = wordlist[num]
+  if (!word) throw new Error(`Invalid word number: ${num}`)
+  return word
+}
 
-const truncate = curry((len: number, str: string) => {
-  return str.slice(0, len)
+const leftPad = curry((filler: string, len: number, item: string) => {
+  let result = item
+  while (result.length < len) result = filler + result
+  return result
 })
+
+// const zeroPad = leftPad('0')
+
+// const truncate = curry((len: number, str: string) => {
+//   return str.slice(0, len)
+// })
 
 function parseBin (str: string) {
   return parseInt(str, 2)
 }
 
-function inGroupsOf (n: number, items: any[]) {
-  const groups: any[][] = []
-  let count = 0
-  let group: any[] = []
-  items.forEach((item) => {
-    group.push(item)
-    count += 1
-    if (count >= n) {
-      groups.push(group)
-      group = []
-      count = 0
-    }
-  })
-  return groups
+// export function parseWords (words: string[]): Uint8Array {
+//   const asBin = words
+//     .map(toNum)
+//     .map(toBin)
+//     .map(zeroPad(11))
+//   const byteNums = inGroupsOf(3, asBin)
+//     .map((g) => g.join(''))
+//     .map(truncate(32))
+//     .map(splitEvery(8))
+//     .map((b8s) => b8s.map(parseBin))
+//     .reduce((all, nums) => all.concat(nums), [])
+//   return new Uint8Array(byteNums)
+// }
+
+// export function parseWordsChecksum (words: string[]): Uint8Array {
+//   const checksumWord = words[words.length - 1]
+//   const toParse = words.slice(0, words.length - 1)
+//   const bytes = parseWords(toParse)
+//   const checksum = bytes.reduce((a, n) => a + n, 0) % 2048
+//   if (checksum !== toNum(checksumWord)) {
+//     throw new Error(`Checksum mismatch: "${checksumWord}"`)
+//   }
+//   return bytes
+// }
+
+// export function encodeBytes (bytes: Uint8Array): string[] {
+//   return inGroupsOf(4, Array.from(bytes))
+//     .map((g) => g.map(zeroPad(8)).join(''))
+//     .map((b32) => b32 + '0')
+//     .map(splitEvery(11))
+//     .map((b11s) => b11s.map(parseBin))
+//     .reduce((all, group) => all.concat(group), [])
+//     .map((wordNum) => wordlist[wordNum])
+// }
+
+function ensureLength4 (nums: number[]): number[] {
+  while (nums.length < 4) nums.push(0x00)
+  return nums
 }
 
-const splitEvery = curry((n: number, str: string) => {
-  let pos = 0
-  const chunks = []
-  while (pos < str.length) {
-    chunks.push(str.slice(pos, n + pos))
-    pos += n
-  }
-  return chunks
-})
-
-export function parseWords (words: string[]): Uint8Array {
-  const asBin = words
-    .map(toNum)
-    .map(toBin)
-    .map(zeroPad(11))
-  const byteNums = inGroupsOf(3, asBin)
-    .map((g) => g.join(''))
-    .map(truncate(32))
-    .map(splitEvery(8))
-    .map((b8s) => b8s.map(parseBin))
-    .reduce((all, nums) => all.concat(nums), [])
-  return new Uint8Array(byteNums)
-}
-
-export function parseWordsChecksum (words: string[]): Uint8Array {
-  const checksumWord = words[words.length - 1]
-  const toParse = words.slice(0, words.length - 1)
-  const bytes = parseWords(toParse)
-  const checksum = bytes.reduce((a, n) => a + n, 0) % 2048
-  if (checksum !== toNum(checksumWord)) {
-    throw new Error(`Checksum mismatch: "${checksumWord}"`)
-  }
-  return bytes
+function checkWord (bytes: Uint8Array): string {
+  const dropNum = 2 ** DROP_BITS
+  const toDrop = (dropNum - (bytes.length % dropNum)) % dropNum
+  const mod = 2 ** CHECKSUM_BITS
+  const checksum = sum(Array.from(bytes)) % mod
+  const dropBits = toBin(2, toDrop)
+  const csBits = toBin(9, checksum)
+  const checkBits = dropBits + csBits
+  const checkNum = parseBin(checkBits)
+  return toWord(checkNum)
 }
 
 export function encodeBytes (bytes: Uint8Array): string[] {
-  return inGroupsOf(4, Array.from(bytes))
-    .map((g) => g.map(zeroPad(8)).join(''))
-    .map((b32) => b32 + '0')
-    .map(splitEvery(11))
-    .map((b11s) => b11s.map(parseBin))
-    .reduce((all, group) => all.concat(group), [])
-    .map((wordNum) => wordlist[wordNum])
+  const cw = checkWord(bytes)
+  const nums = Array.from(bytes)
+  const bin33s = pipe(
+    splitEvery(4),
+    map(ensureLength4),
+    map(map(toBin(8))),
+    map(reduce((b32, b8) => b32 + b8, '')),
+    map((b32) => b32 + '0')
+  )(nums)
+  if (!all((bin33) => bin33.length === 33, bin33s)) {
+    throw new Error(
+      `Expected all 3-word binary chunks to have length 33, got: ${bin33s}`
+    )
+  }
+  const bin11s = pipe(
+    map(split('')),
+    map(splitEvery(11)),
+    map(map(join(''))),
+    unnest
+  )(bin33s)
+  const words = bin11s.map(parseBin).map(toWord)
+  if (words.length % 3 !== 0) {
+    throw new Error(
+      `Expected data word count to be a multiple of 3, got ${
+        words.length
+      } words`
+    )
+  }
+
+  return words.concat(cw)
 }
