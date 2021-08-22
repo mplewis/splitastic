02fc0c6b15d91ba3443ed3d5d7f09affa154ead3
GateQuality <ggitenzo@gmail.com>
2021-08-22 22:06:51 +0000

    add test

--- src/services/wordlist.test.ts
+++ src/services/wordlist.test.ts
@@ -1,6 +1,14 @@
-import { range } from 'ramda'
-import { decodeWords, encodeBytes } from './wordlist'
+import rewire from "rewire"
+import { range } from "ramda"
+import { decodeWords, encodeBytes } from "./wordlist"
 
+const wordlist = rewire("@/services/wordlist")
+const toWord = wordlist.__get__("toWord")
+const toNum = wordlist.__get__("toNum")
+const parseBin = wordlist.__get__("parseBin")
+const ensureLength4 = wordlist.__get__("ensureLength4")
+const genCheckWord = wordlist.__get__("genCheckWord")
+const checkVals = wordlist.__get__("checkVals")
 describe('encodeBytes', () => {
   it('encodes bytes including the proper checkword', () => {
     const toEncode = new Uint8Array([0xc0, 0xff, 0xee, 0xca, 0xfe])
@@ -97,4 +105,400 @@ describe('symmetry stress test', () => {
       expect(decoded).toEqual(data)
     })
   })
+})
+
+// @ponicode
+describe("toWord", () => {
+    test("0", () => {
+        let callFunction: any = () => {
+            toWord(0)
+        }
+    
+        expect(callFunction).not.toThrow()
+    })
+
+    test("1", () => {
+        let callFunction: any = () => {
+            toWord(100)
+        }
+    
+        expect(callFunction).not.toThrow()
+    })
+
+    test("2", () => {
+        let callFunction: any = () => {
+            toWord(-100)
+        }
+    
+        expect(callFunction).not.toThrow()
+    })
+
+    test("3", () => {
+        let callFunction: any = () => {
+            toWord(-1)
+        }
+    
+        expect(callFunction).not.toThrow()
+    })
+
+    test("4", () => {
+        let callFunction: any = () => {
+            toWord(1)
+        }
+    
+        expect(callFunction).not.toThrow()
+    })
+
+    test("5", () => {
+        let callFunction: any = () => {
+            toWord(NaN)
+        }
+    
+        expect(callFunction).not.toThrow()
+    })
+})
+
+// @ponicode
+describe("toNum", () => {
+    test("0", () => {
+        let callFunction: any = () => {
+            toNum("ea")
+        }
+    
+        expect(callFunction).not.toThrow()
+    })
+
+    test("1", () => {
+        let callFunction: any = () => {
+            toNum("et")
+        }
+    
+        expect(callFunction).not.toThrow()
+    })
+
+    test("2", () => {
+        let callFunction: any = () => {
+            toNum("natus")
+        }
+    
+        expect(callFunction).not.toThrow()
+    })
+
+    test("3", () => {
+        let callFunction: any = () => {
+            toNum("eveniet")
+        }
+    
+        expect(callFunction).not.toThrow()
+    })
+
+    test("4", () => {
+        let callFunction: any = () => {
+            toNum("voluptas")
+        }
+    
+        expect(callFunction).not.toThrow()
+    })
+
+    test("5", () => {
+        let callFunction: any = () => {
+            toNum("")
+        }
+    
+        expect(callFunction).not.toThrow()
+    })
+})
+
+// @ponicode
+describe("parseBin", () => {
+    test("0", () => {
+        let callFunction: any = () => {
+            parseBin("<?xml version=\"1.0\" ?>\n<a b=\"c\"/>\n")
+        }
+    
+        expect(callFunction).not.toThrow()
+    })
+
+    test("1", () => {
+        let callFunction: any = () => {
+            parseBin("<?xml version=\"1.0\" ?><a b=\"c\"/>")
+        }
+    
+        expect(callFunction).not.toThrow()
+    })
+
+    test("2", () => {
+        let callFunction: any = () => {
+            parseBin("")
+        }
+    
+        expect(callFunction).not.toThrow()
+    })
+})
+
+// @ponicode
+describe("ensureLength4", () => {
+    test("0", () => {
+        let callFunction: any = () => {
+            ensureLength4([1])
+        }
+    
+        expect(callFunction).not.toThrow()
+    })
+
+    test("1", () => {
+        let callFunction: any = () => {
+            ensureLength4([-5.48, -100, 100])
+        }
+    
+        expect(callFunction).not.toThrow()
+    })
+
+    test("2", () => {
+        let callFunction: any = () => {
+            ensureLength4([100, 0, -5.48, 1, -5.48])
+        }
+    
+        expect(callFunction).not.toThrow()
+    })
+
+    test("3", () => {
+        let callFunction: any = () => {
+            ensureLength4([-5.48, -5.48, 0, 100, -5.48])
+        }
+    
+        expect(callFunction).not.toThrow()
+    })
+
+    test("4", () => {
+        let callFunction: any = () => {
+            ensureLength4([100, 0])
+        }
+    
+        expect(callFunction).not.toThrow()
+    })
+
+    test("5", () => {
+        let callFunction: any = () => {
+            ensureLength4([])
+        }
+    
+        expect(callFunction).not.toThrow()
+    })
+})
+
+// @ponicode
+describe("genCheckWord", () => {
+    test("0", () => {
+        let param1: any = new Uint8Array([-1, -1, 100, 100, 0, -1, -100, 0, 100])
+        let callFunction: any = () => {
+            genCheckWord(param1)
+        }
+    
+        expect(callFunction).not.toThrow()
+    })
+
+    test("1", () => {
+        let param1: any = new Uint8Array([-1, 1, -1])
+        let callFunction: any = () => {
+            genCheckWord(param1)
+        }
+    
+        expect(callFunction).not.toThrow()
+    })
+
+    test("2", () => {
+        let param1: any = new Uint8Array([0, 0, -1, -100, -100, -1, 1, -1])
+        let callFunction: any = () => {
+            genCheckWord(param1)
+        }
+    
+        expect(callFunction).not.toThrow()
+    })
+
+    test("3", () => {
+        let param1: any = new Uint8Array([1, -1, 1, 0, 1, 0, 100, 100, -100])
+        let callFunction: any = () => {
+            genCheckWord(param1)
+        }
+    
+        expect(callFunction).not.toThrow()
+    })
+
+    test("4", () => {
+        let param1: any = new Uint8Array([-100, 1, 1, -100, -100, 0, 1, 0, -100])
+        let callFunction: any = () => {
+            genCheckWord(param1)
+        }
+    
+        expect(callFunction).not.toThrow()
+    })
+
+    test("5", () => {
+        let param1: any = new Uint8Array([])
+        let callFunction: any = () => {
+            genCheckWord(param1)
+        }
+    
+        expect(callFunction).not.toThrow()
+    })
+})
+
+// @ponicode
+describe("wordlist.encodeBytes", () => {
+    test("0", () => {
+        let param1: any = new Uint8Array([100, -100, 1, 100, 1, -100])
+        let callFunction: any = () => {
+            wordlist.encodeBytes(param1)
+        }
+    
+        expect(callFunction).not.toThrow()
+    })
+
+    test("1", () => {
+        let param1: any = new Uint8Array([-100, -1, -100, -1, -100, -100, -1, 1, 100])
+        let callFunction: any = () => {
+            wordlist.encodeBytes(param1)
+        }
+    
+        expect(callFunction).not.toThrow()
+    })
+
+    test("2", () => {
+        let param1: any = new Uint8Array([-1, -100, 0, 0, 1, 0, -100, -1])
+        let callFunction: any = () => {
+            wordlist.encodeBytes(param1)
+        }
+    
+        expect(callFunction).not.toThrow()
+    })
+
+    test("3", () => {
+        let param1: any = new Uint8Array([1, 0, -1, -100, 1, -1, -1, 1])
+        let callFunction: any = () => {
+            wordlist.encodeBytes(param1)
+        }
+    
+        expect(callFunction).not.toThrow()
+    })
+
+    test("4", () => {
+        let param1: any = new Uint8Array([1, -1, 1, 0, -100, -100, 0, -1])
+        let callFunction: any = () => {
+            wordlist.encodeBytes(param1)
+        }
+    
+        expect(callFunction).not.toThrow()
+    })
+
+    test("5", () => {
+        let param1: any = new Uint8Array([])
+        let callFunction: any = () => {
+            wordlist.encodeBytes(param1)
+        }
+    
+        expect(callFunction).not.toThrow()
+    })
+})
+
+// @ponicode
+describe("checkVals", () => {
+    test("0", () => {
+        let callFunction: any = () => {
+            checkVals("aniyah")
+        }
+    
+        expect(callFunction).not.toThrow()
+    })
+
+    test("1", () => {
+        let callFunction: any = () => {
+            checkVals("sammy")
+        }
+    
+        expect(callFunction).not.toThrow()
+    })
+
+    test("2", () => {
+        let callFunction: any = () => {
+            checkVals("claudie")
+        }
+    
+        expect(callFunction).not.toThrow()
+    })
+
+    test("3", () => {
+        let callFunction: any = () => {
+            checkVals("aleen")
+        }
+    
+        expect(callFunction).not.toThrow()
+    })
+
+    test("4", () => {
+        let callFunction: any = () => {
+            checkVals("gunner")
+        }
+    
+        expect(callFunction).not.toThrow()
+    })
+
+    test("5", () => {
+        let callFunction: any = () => {
+            checkVals("")
+        }
+    
+        expect(callFunction).not.toThrow()
+    })
+})
+
+// @ponicode
+describe("wordlist.decodeWords", () => {
+    test("0", () => {
+        let callFunction: any = () => {
+            wordlist.decodeWords(["necessitatibus dolor nemo", "necessitatibus dolor nemo", "necessitatibus dolor nemo", "in et tempore", "perferendis aut voluptatibus"])
+        }
+    
+        expect(callFunction).not.toThrow()
+    })
+
+    test("1", () => {
+        let callFunction: any = () => {
+            wordlist.decodeWords(["consectetur blanditiis rerum", "in et tempore", "illo qui omnis", "perferendis aut voluptatibus"])
+        }
+    
+        expect(callFunction).not.toThrow()
+    })
+
+    test("2", () => {
+        let callFunction: any = () => {
+            wordlist.decodeWords(["consectetur blanditiis rerum", "perferendis aut voluptatibus", "perferendis aut voluptatibus", "necessitatibus dolor nemo"])
+        }
+    
+        expect(callFunction).not.toThrow()
+    })
+
+    test("3", () => {
+        let callFunction: any = () => {
+            wordlist.decodeWords(["in et tempore", "in et tempore", "perferendis aut voluptatibus", "in et tempore"])
+        }
+    
+        expect(callFunction).not.toThrow()
+    })
+
+    test("4", () => {
+        let callFunction: any = () => {
+            wordlist.decodeWords(["perferendis aut voluptatibus"])
+        }
+    
+        expect(callFunction).not.toThrow()
+    })
+
+    test("5", () => {
+        let callFunction: any = () => {
+            wordlist.decodeWords([])
+        }
+    
+        expect(callFunction).not.toThrow()
+    })
 })
